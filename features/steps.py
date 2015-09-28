8ca5074a47d95cfcbd919513032452bff348745c
Manuel Velez Martinez <manuelvelez@gmail.com>
2015-09-28 10:41:40 +0200

    changes in configElement

--- features/steps.py
+++ features/steps.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.by import By
 
 @step(u'Given ikabo is opened')
 def given_ikabo_is_opened(step):
-	time.sleep(0)
+	time.sleep(2)
 
 @step(u'When I configure valo with "([^"]*)"')
 def when_i_configure_valo_with_group1(step, group1):
@@ -32,7 +32,7 @@ def and_click_start_button(step):
 def then_i_can_see_the_main_screen_with(step, url, tenant):
 	time.sleep(1)
 
-	configElement = world.driver.find_element_by_xpath('//*[@id="mod-user-1"]/div').text
+	configElement = world.driver.find_element_by_xpath('//*[@id="mod-user-1"]/div[@data-id="profile"]').text
 	assert configElement == url + " " + tenant
 
 	streamLists = world.driver.find_elements_by_css_selector("[class=description]")
